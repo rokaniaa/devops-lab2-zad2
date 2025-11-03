b1ec9b97eaeea8ab30a92c43c5e5e7247000bbd5
rokaniaa <rokaniaa@student.wszib.edu.pl>
2025-11-03 20:27:37 +0100

    add docs directory with sample test

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTests {
+
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
